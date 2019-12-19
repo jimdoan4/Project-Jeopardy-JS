46acdd97558ea9d2c817f63cc4862360e6c41965
Jim Doan <jimdoan4@gmail.com>
2019-12-19 13:20:38 -0500

    added bootstrap and styled header and individual boxes

---
+++ main.js
@@ -0,0 +1,33 @@
+var questions = [
+  /* HTML Category 1 Questions */
+    {
+      prompt: 'What is the expansion of HTML?',
+      options: [ 'Hyper Text Markup Language', 'Holding Text Marker Language', 'Hyperlink Text Management Language', 'Holding Text Modal Language'],
+      correctAnswer: 'Hyper Text Markup Language',
+      points: 10  },
+    {
+      prompt: 'What is the current version of HTML?',
+      options: ['HTMLv5.1', 'HTML5', 'HTML2016', 'HTMLC'],
+      correctAnswer: 'HTML5',
+      points: 50
+    },
+    {
+      prompt: 'How to apply Hyperlink in an HTML page?',
+      options: ['<ahref> tag', '<p> tag', '<input> tag', '<h1> tag'],
+      correctAnswer: '<ahref> tag',
+      points: 100
+    },
+    {
+      prompt: 'What are the storage types of HTML5?',
+      options: ['Session Storage', 'Main Storage', 'Varied Storage', 'Storage Notation'],
+      correctAnswer: 'Session Storage',
+      points: 500
+    },
+    {
+      prompt: 'HTML is the principal source for attributes of Nodes in the DOM. Browsers do not usually allow cross-site actions but there is one major exception:',
+      options: ['Get Requests', 'Ping Commands', 'iFrames', 'XSS '],
+      correctAnswer: 'iFrames',
+      points: 1000
+    }
+  ];
+  
